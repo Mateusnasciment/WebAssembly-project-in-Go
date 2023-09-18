9134f9cdac195ffcbbdecbe6ea4d1be8ba6b5600
Mateus nascimento <mateus.nasciment@hotmail.com>
2023-09-18 07:35:50 -0300

    feat:webassmblye

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/bradcypert/go-wasm
 
 go 1.21.1
+
+require (
+	github.com/shurcooL/go v0.0.0-20200502201357-93f07166e636 // indirect
+	github.com/shurcooL/go-goon v1.0.0 // indirect
+	github.com/shurcooL/goexec v0.0.0-20230709021537-96bada04ea2b // indirect
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/tools v0.13.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,9 +2,26 @@ package main
 
 import (
 	"fmt"
-	import "github.com/shurcooL/goexec"
+	"syscall/js"
 )
 
-func main () {
-	fmt.Println("Hello World")
+func main() {
+	fmt.Println("Hello, WebAssembly!")
+
+	document := js.Global().Get("document")
+	p := document.Call("createElement", "p")
+	p.Set("innerHTML", "Hello, WebAssembly!")
+	document.Get("body").Call("appendChild", p)
+	p.Set("className", "block")
+
+	styles := document.Call("createElement", "style")
+	styles.Set("innerHTML", `
+		.block {
+			background-color: #f00;
+			color: #fff;
+			padding: 1em;
+		}
+	`)
+	document.Get("head").Call("appendChild", styles)
+	document.Get("body").Call("appendChild", p)
 }
